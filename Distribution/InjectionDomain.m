69be2c95faaabbef01ff0f0961ca9bf157dc89a0
Anthony DeGennaro <adegenna@princeton.edu>
2015-02-17 20:12:31 -0500

    Lots of development on InjectionDomain

--- Distribution/InjectionDomain.m
+++ Distribution/InjectionDomain.m
@@ -12,95 +12,110 @@ classdef InjectionDomain < hgsetget
         fu; fv; % Velocity
         fR; % Radius
         fe; % Energy
-        % Total mass of domain
-        mass;
+        ft; % Time
+        % Ensemble average quantities of the domain
+        LWC_avg;
+        mass_avg;
+        R_avg;
+        numDroplets_avg;
+        dtTraverse_avg;
         % Parameters for sampling f(x,u,R,e) over the domain
         samples;
+        simTime;
         x = []; y = [];
         u = []; v = [];
         R = []; e = [];
-        n = [];
+        nDroplet = [];
+        t = [];
     end
     
     methods
         
-        function domain = InjectionDomain(dx,strPDFTypes,PDFparams,fluid,airfoil)
+        function domain = InjectionDomain(strPDFTypes,PDFparams,fluid,airfoil,LWC_avg,simTime)
             % Constructor
             % INPUTS:
-            %   dx: length scale for 'calcInjectionDomain'
             %   strPDFTypes: Vector of strings for 'setPDF'
             %   PDFparams: 2-column array of parameters for 'setPDF'
+            %   LWC_avg: ensemble average for LWC
             
             % Set PDFs for u,v,R,e
             domain.fu = domain.setPDF(strPDFTypes(1),PDFparams(1,:));
             domain.fv = domain.setPDF(strPDFTypes(2),PDFparams(2,:));
             domain.fR = domain.setPDF(strPDFTypes(3),PDFparams(3,:));
             domain.fe = domain.setPDF(strPDFTypes(4),PDFparams(4,:));
-            
-            % Calculate bounds of injection domain
+            domain.ft = domain.setPDF(strPDFTypes(5),PDFparams(5,:));
+            domain.LWC_avg = LWC_avg;
+            domain.simTime = simTime;
+            % Set fluid density of droplets
+            domain.rhol = fluid.rhol;
+            % Calculate bounds of injection domain and avg time to traverse
             if strcmp(strPDFTypes(3),'Gaussian')==1
                 Ravg = PDFparams(3,1);
             elseif strcmp(strPDFTypes(3),'Uniform')==1
                 Ravg = 0.5*(PDFparams(3,1)+PDFparams(3,2));
             end
-            domain.calcInjectionDomain(dx,Ravg,fluid,airfoil);
+            domain.R_avg = Ravg;
+            domain.calcInjectionDomain(fluid,airfoil);
+            % Calculate area of trapezoidal injection domain
+            domain.calcDomainArea();
+            % Calculate the average droplet mass from the distribution f(R)
+            R = domain.fR(:,1); fR = domain.fR(:,2);
+            Integrand = 4/3*pi*domain.rhol*(R.^3);
+            md_avg = trapz(R,Integrand.*fR);
+            % Calculate average number of total droplets in the simulation
+            % (assumes differential thickness of domain = one MVD)
+            ThicknessSlice = 2*domain.R_avg;
+            VolSlice = ThicknessSlice*domain.area;
+            I_t = simTime/domain.dtTraverse_avg;
+            domain.numDroplets_avg = I_t*LWC_avg*VolSlice/md_avg;
+            % Set spatial PDF as uniform in space over the trapezoidal
+            % domain, scaled by the average number of particles
+            domain.fxy = @(xq,yq) domain.num_avg*inpolygon(xq,yq,xv,yv)./domain.area;
+        end
+        
+        function sampleRealization(numClumps)
+            % Function to create a realization based on desired number of
+            % particle clumps and simulation time
+            % INPUTS:
+            %   numClumps: desired number of particle clumps
+            %   simTime: desired time of simulation
             
-            xy = domain.XY_bounds;
-            xv = [xy(1:2,1); xy(4,1); xy(3,1)];
-            yv = [xy(1:2,2); xy(4,2); xy(3,2)];
-            % Calculate area of trapezoidal domain
-            xREC = linspace(min(xy(:,1)),max(xy(:,1)),500)';
-            yREC = linspace(min(xy(:,2)),max(xy(:,2)),500)';
-            dx = xREC(2)-xREC(1);
-            dy = yREC(2)-yREC(1);
-            [XX,YY] = meshgrid(xREC,yREC);
-            X = XX(:); Y = YY(:);
-            area = sum(inpolygon(X,Y,xv,yv))*(dx*dy);
-            domain.area = area;
+            % Code to draw random samples from (x,u,R,e;t)
+            % ...
+            % Code to evaluate f(x,u,R,e;t) at sample points
+            % ...
             
-            % Set spatial PDF as uniform in space over the trapezoidal domain
-            domain.fxy = @(xq,yq) inpolygon(xq,yq,xv,yv)./area;
-            % Set fluid density of droplets
-            domain.rhol = fluid.rhol;
+            dV = domain.numDroplets_avg/sum(fEval.*mEval);
+            domain.nDroplet = fEval.*dV;
             
         end
         
-        function calcInjectionDomain(domain,dx,Ravg,fluid,airfoil)
+        function calcInjectionDomain(domain,fluid,airfoil)
             % Function to calculate a box which contains the droplet clusters
             % INPUTS: 
-            %   dx: length scale (desired domain length in the x-direction)
-            %   Ravg: average droplet radius
             %   fluid,airfoil: fluid and airfoil objects
-
+            
+            Ravg = domain.R_avg;
             % Determine impingement limits in y-direction at x0
             xL = -0.5; Yhit = -0.1;
             Ymiss = 0.3;
             yLimUP = impingementLimitsSLD(Ravg,fluid,airfoil,xL,Ymiss,Yhit,'UP');
             Ymiss = -0.3;
             yLimDOWN = impingementLimitsSLD(Ravg,fluid,airfoil,xL,Ymiss,Yhit,'DOWN');
-
-            % Initialize cloud object of 2 particles placed at impingement limits
-            x0 = [xL; xL];
-            y0 = [yLimUP; yLimDOWN];
-            [pg,ug,vg] = interpFluid(fluid,x0,y0);
-            u0 = ug + 0.01*ug;
-            v0 = vg + 0.01*vg;
-            rd0 = [Ravg; Ravg];
-            time0 = [0; 0];
-            particles = 2;
-            cloud = SLDcloud([x0 y0 u0 v0 rd0 time0 [1:particles]'],fluid.rhol,particles);
-
-            % Advect particles backwards for time dx/U
-            Uinf = fluid.Uinf;
-            tStar = -dx/Uinf;
-            while max(cloud.time) > tStar
-                calcDtandImpinge(cloud,airfoil,fluid);
-                set(cloud,'dt',-cloud.dt);
-                transportSLD(cloud,fluid);
-            end
-
+            x1 = [xL; yLimUP];
+            x2 = [xL; yLimDOWN];
+            % Determine impingement limits in y-direction at x = x0-1
+            xL = xL-1; Yhit = -0.25;
+            Ymiss = 0.3;
+            yLimUP = impingementLimitsSLD(Ravg,fluid,airfoil,xL,Ymiss,Yhit,'UP');
+            Ymiss = -0.35;
+            yLimDOWN = impingementLimitsSLD(Ravg,fluid,airfoil,xL,Ymiss,Yhit,'DOWN');
+            x3 = [xL; yLimUP];
+            x4 = [xL; yLimDOWN];
             % Save new boundaries of particles
-            domain.XY_bounds = [x0 y0; cloud.x cloud.y];
+            domain.XY_bounds = [x1'; x2'; x3'; x4'];
+            % Estimate time to traverse domain
+            domain.dtTraverse_avg = 1/fluid.Uinf;
         end
         
         function pdf = setPDF(domain,strType,params)
@@ -113,7 +128,7 @@ classdef InjectionDomain < hgsetget
                 % 'params' = [mu sigma]
                 mu = params(1);
                 sigma = params(2);
-                xsamp = linspace(mu-3*sigma,mu+3*sigma,1000)';
+                xsamp = linspace(mu-5*sigma,mu+5*sigma,3000)';
                 func = 1/sigma/sqrt(2*pi)*exp(-0.5*(xsamp-mu).^2./(sigma^2));
             elseif strcmp(strType,'Uniform')
                 % 'params' = [minx maxx]
@@ -126,23 +141,35 @@ classdef InjectionDomain < hgsetget
             pdf = [xsamp, func];
         end
         
-        function integral = integratePDF(domain,strI,fluid)
-            % Function to compute an integral over the domain, weighted by
-            % the number density function
+        function calcDomainArea(domain)
+            % Function to compute the area of the injection domain
+            
+            xy = domain.XY_bounds;
+            xv = [xy(1:2,1); xy(4,1); xy(3,1)];
+            yv = [xy(1:2,2); xy(4,2); xy(3,2)];
+            xREC = linspace(min(xy(:,1)),max(xy(:,1)),500)';
+            yREC = linspace(min(xy(:,2)),max(xy(:,2)),500)';
+            dx = xREC(2)-xREC(1);
+            dy = yREC(2)-yREC(1);
+            [XX,YY] = meshgrid(xREC,yREC);
+            X = XX(:); Y = YY(:);
+            area = sum(inpolygon(X,Y,xv,yv))*(dx*dy);
+            domain.area = area;
+            
+        end
+        
+        function calcAvgMass(domain)
+            % Function to compute the average mass of the injection domain
             % INPUTS:
             %   'strI': string defining type of integrand
-            %   'fluid': object defining properties of fluid
-                
+            
             rhol = domain.rhol;
-            if strcmp(strI,'Mass')
-                R = domain.fR(:,1); fR = domain.fR(:,2);
-                Integrand = 4/3*pi*rhol*(R.^3);
-                I_mass = trapz(R,Integrand.*fR);
-                u = domain.fu(:,1); fu = domain.fu(:,2); I_u = trapz(u,fu);
-                v = domain.fv(:,1); fv = domain.fv(:,2); I_v = trapz(v,fv);
-                e = domain.fe(:,1); fe = domain.fe(:,2); I_e = trapz(e,fe);
-                integral = domain.NDOM.*I_mass.*I_u.*I_v.*I_e;
-            end
+            I_xy = domain.num_avg;
+            I_u = 1; I_v = 1; I_e = 1;
+            R = domain.fR(:,1); fR = domain.fR(:,2);
+            Integrand = 4/3*pi*rhol*(R.^3);
+            I_mass = trapz(R,Integrand.*fR);
+            domain.mass_avg = I_mass.*I_xy.*I_u.*I_v.*I_e;
             
         end
         
