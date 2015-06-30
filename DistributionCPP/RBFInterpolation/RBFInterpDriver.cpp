aa0552a0d5769f9ab039acfc11bbcf2cbd2c5caf
adegenna <adegenna@princeton.edu>
2015-06-30 10:28:10 -0400

    Minor tweaks to RBF test driver

--- DistributionCPP/RBFInterpolation/RBFInterpDriver.cpp
+++ DistributionCPP/RBFInterpolation/RBFInterpDriver.cpp
@@ -11,22 +11,26 @@
 using namespace std;
 
 int main(int argc, const char *argv[]) {
-  int N = 5;
+  int N = 20;
   int d = 2;
   Eigen::MatrixXd X(d,N);
   Eigen::VectorXd Y(N);
-  X << 0,0,1,1,0.5,
-    0,1,0,1,0.5;
-  Y << 0,1,1,2,0;
+  X << 0,0,0,0,.25,.25,.25,.25,.5,.5,.5,.5,.75,.75,.75,.75,1,1,1,1,
+    0,.25,.5,.75,1,0,.25,.5,.75,1,0,.25,.5,.75,1,0,.25,.5,.75,1;
+  Eigen::Vector2d Mean;
+  Mean << 0.7,0.6;
+  for (int i=0; i<N; i++) {
+    Y(i) = exp(-((X.col(i)-Mean).squaredNorm()));
+  }
   RBFInterpolant spline = RBFInterpolant(X,Y);
   spline.calcInterpolationWeights();
   Eigen::VectorXd Xq(2);
   // Interpolate on a grid
   int Xres = 10; double dx = 1.0/(Xres-1);
   int Yres = 10; double dy = 1.0/(Yres-1);
-  std::vector<double> Xgrid(Xres*Yres);
-  std::vector<double> Ygrid(Xres*Yres);
-  std::vector<double> Zgrid(Xres*Yres);
+  std::vector<double> Xgrid;
+  std::vector<double> Ygrid;
+  std::vector<double> Zgrid;
   double Xtmp = 0;
   double Ytmp = 0;
   double zq;
